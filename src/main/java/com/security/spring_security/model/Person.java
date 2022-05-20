9054955210f3655c894fb17a03104de1b660fb1e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-05-20 18:26:51 +0200

    added model

--- src/main/java/com/security/spring_security/model/Person.java
+++ src/main/java/com/security/spring_security/model/Person.java
@@ -1,14 +1,32 @@
 package com.security.spring_security.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+@Data
+@NoArgsConstructor
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "year_of_birth")
     private int yearOfBirth;
 
+    @Column(name = "password")
     private String password;
 
-
+    public Person(String username, int yearOfBirth, String password) {
+        this.username = username;
+        this.yearOfBirth = yearOfBirth;
+        this.password = password;
+    }
 }
