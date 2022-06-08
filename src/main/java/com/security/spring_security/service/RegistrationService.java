09338f3dff2d1a57ec7ee31dd753c9eb00b32be6
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-06-08 18:27:41 +0200

    added BCryptPasswordEncoder

--- src/main/java/com/security/spring_security/service/RegistrationService.java
+++ src/main/java/com/security/spring_security/service/RegistrationService.java
@@ -3,6 +3,7 @@ package com.security.spring_security.service;
 import com.security.spring_security.model.Person;
 import com.security.spring_security.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final PersonRepository repository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public RegistrationService(PersonRepository repository) {
+    public RegistrationService(PersonRepository repository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.repository = repository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        var encodedPassword = bCryptPasswordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
         repository.save(person);
     }
 }
